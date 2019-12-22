b5c40b052f83c9e1b30cfa19c6fe4ca0f1d4b14b
MatteoDaccordo <matteo.daccordo@gmail.com>
2019-12-22 12:50:03 +0100

    Hello world

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+const title = "\n\n\n\
+_____                __      __\n\
+|  __ \\              | |     | |\n\
+| |__) |___  __ _  __| |_   _| |\n\
+|  _  // _ \\/ _` |\/ _` | | | | |\n\
+| | \\ \\  __/ (_| | (_| | |_| |_|\n\
+|_|  \\_\\___|\\__,_|\\__,_|\\__, (_)\n\
+                        __/ |  \n\
+                       |___/   \n\n\
+";
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+console.log(title);
